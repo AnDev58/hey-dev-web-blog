796b823f262ea7d7fbe1d6d6b46ddd5fc37a9e0c
MB-Proger <miginandrey@outlook.com>
2021-12-08 15:42:20 +0300

    Created basic 'Hello, World!' server

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	r := mux.NewRouter()
-	r.Host(":8080")
+	r.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Hello World")
+	}).Methods("GET")
+	srv := http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
